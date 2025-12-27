81896fd39f2356df8336608072abd79f7cba412c
Gabriella-Sipos <gabriellasipos30@gmail.com>
2025-12-27 13:04:32 +0100

    Update Login.vue
    Be-/kijelentkezés

--- Frontend/src/pages/Login.vue
+++ Frontend/src/pages/Login.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
+
+    <Admin v-if="isLoggedIn && currentUser?.role==='ADMIN'" @logged-out="handleLogout" />
+
     <!-- Ha be van jelentkezve a user -->
-    <UserProfile v-if="isLoggedIn" @logged-out="handleLogout" />
+    <Reservation v-else-if="isLoggedIn && currentUser?.role === 'USER'" @logged-out="handleLogout" />
 
     <!-- Ha nincs bejelentkezve -->
     <AuthForm v-else @logged-in="handleLogin" />
@@ -9,20 +12,56 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import AuthForm from './components/AuthForm.vue'
-import UserProfile from './components/UserProfile.vue'
+import Reservation from '../pages/Reservation.vue'
+import Admin from '../pages/Admin.vue'
 
-const isLoggedIn = ref(!!localStorage.getItem('token'))
+const isLoggedIn = ref(!!localStorage.getItem('token'));
+const currentUser= ref([]);
 
 // Bejelentkezés esemény kezelése
-const handleLogin = () => {
+const handleLogin = async() => {
   isLoggedIn.value = true
+  await fetchMe();
 }
 
 // Kijelentkezés esemény kezelése
 const handleLogout = () => {
   localStorage.removeItem('token')
   isLoggedIn.value = false
+  currentUser.value = null
 }
+
+const fetchMe = async () => {
+  if (!isLoggedIn.value) return;
+  try {
+    const res = await fetch("http://localhost:3300/me", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    if (!res.ok) {
+      emit("logged-out");
+      return;
+    }
+    currentUser.value = await res.json();
+    console.log("Users adatok",currentUser.value,currentUser.value.role)
+  } catch (err) {
+    console.error("Nem sikerült lekérni a profilt:", err);
+  }
+};
+
+onMounted(()=>{
+  fetchMe();}
+);
+
+watch(isLoggedIn, (loggedIn) => {
+  if (loggedIn) {
+    fetchMe();
+  } else {
+    currentUser.value = null;
+  }
+});
+
 </script>
