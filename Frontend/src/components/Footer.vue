d926cd0f2adb3aba9bdfb187e3473b951a8566c0
Szabics Tibor <szabicst@gmail.com>
2026-01-03 21:08:20 +0100

    Create Footer.vue

---
+++ Frontend/src/components/Footer.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+defineProps({
+  openningHours: Array
+});
+</script>
+
+<template>
+  <footer class="bg-info text-white py-5">
+    <div class="container text-center">
+      <div class="mb-3">
+        <i class="fa-solid fa-utensils fa-2x"></i>
+        <span class="h4 ms-2 fw-bold text-outline ">The Table</span>
+      </div>
+      <nav class="mb-3">
+       <RouterLink class="text-white mx-2" to="/">Főoldal</RouterLink>
+        <RouterLink class="text-white mx-2" to="/#about">Rólunk</RouterLink>
+        <RouterLink class="text-white mx-2" to="/etlap">Étlap</RouterLink>
+        <RouterLink class="text-white mx-2" to="/reservation">Asztalfoglalás</RouterLink>
+        <RouterLink class="text-white mx-2" to="/#contact">Kapcsolat</RouterLink>
+      </nav>
+      <!-- Social icons -->
+      <div class="mb-3">
+        <a href="#" class="text-white mx-1"><i class="fab fa-facebook-f"></i></a>
+        <a href="#" class="text-white mx-1"><i class="fab fa-instagram"></i></a>
+        <a href="#" class="text-white mx-1"><i class="fab fa-tripadvisor"></i></a>
+        <a href="#" class="text-white mx-1"><i class="fab fa-twitter"></i></a>
+      </div>
+      <div id="#contact">
+        <h4 class="fw-bold text-outline">Kapcsolat</h4>
+        <p >Tel.: 06-30/567-4837</p>
+        <p>E-mail: <a href="mailto:thetable@gmail.com"> thetable@gmail.com</a></p>
+        <p>Cím: 6600, Szentes Kossuth Lajos utca 111.</p>
+      </div>
+      
+      <div class="container mt-4">
+        <div class="row justify-content-center">
+          <div class="col-auto">
+            <h5 class="fw-bold text-outline">Nyitvatartás</h5>
+            <ul class="list-unstyled">
+              <li v-for="openningHour in openningHours" :key="openningHour.id">
+                {{ openningHour.day_of_week }}: {{ openningHour.open_time }}:00 - {{ openningHour.close_time }}:00
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="small">&copy; 2004-2026 Minden jog fenntartva</div>
+    </div>
+  </footer>
+</template>
+<style>
+  .text-outline {
+  text-shadow: 2px 2px 3px rgb(42, 145, 134), -1px -1px 3px rgb(42, 145, 134), 1px -1px 3px rgb(42, 145, 134), -1px 1px 3px rgb(42, 145, 134), 1px 1px 3px rgb(42, 145, 134);
+}
+</style>
